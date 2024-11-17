a10f8dc124a0362d03c57cf0e89a1417104f7a0f
prathamdabhi <prathamdabhi456@gmail.com>
2024-11-17 09:35:21 +0530

    Tomato App

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,6 +1,6 @@
 import User from "../models/user.model.js";
 import jwt from 'jsonwebtoken';
-import bcrypt from 'bcrypt';
+import bcrypt from 'bcryptjs';
 import validator from 'validator';
 
 //login user
